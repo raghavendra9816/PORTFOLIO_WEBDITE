bd4845b71fd457756249de47a5ba9e48930a3d64
Achyut Adhikari <raghavendra9816@gmail.com>
2024-06-24 08:47:04 +0545

    portfolio

---
+++ script.js
@@ -0,0 +1,26 @@
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+// menu.onclick = () => {
+//   menu.classList.toggle("bx-x");
+//   navbar.classList.toggle("active");
+// };
+// window.onscroll = () => {
+//   if (navbar.classList.contains("active")) {
+//     menu.classList.remove("bx-x");
+//     navbar.classList.remove("active");
+//   }
+// };
+
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Photographer",
+    "Youtuber",
+  ],
+  typeSpeed: 80,
+  backspeed: 80,
+  backDelay: 1200,
+  loop: true,
+});
